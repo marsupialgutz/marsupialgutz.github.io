b53a727d4446306cbadbf1f74f7259a6f1a6027c
marsupialgutz <electricboy207@gmail.com>
2021-10-24 19:50:07 -0700

    big changes

--- js/dumbthinglol.js
+++
@@ -1,7 +0,0 @@
-function functionTest() {
-    if (document.getElementById("test").textContent == "pspsps click me") {
-      document.getElementById("test").innerHTML = "ur gay";
-    } else if (document.getElementById("test").textContent == "ur gay") {
-       document.getElementById("test").innerHTML = "stop clicking me now"
-    }
-}
